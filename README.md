e665e4533d98132c591ee7c3e554dd56353e74e0
Upul Bandara <upul.bandara@airudi.com>
2020-12-13 22:12:50 -0500

    first end-to-end pipeline completed

---
+++ README.md
@@ -0,0 +1,14 @@
+# Data Modeling with Postgres
+
+In this repository, we create an ETL pipeline to parse user activity data stored in JSON files. Extracted data are stored in a star schema created in a Postgres database. 
+
+## How to Deploy and Run
+
+
+## ETL Pipeline
+
+
+## Database Structure
+
+
+## Conclusions and Future Directions
--- sql_queries.py
+++ sql_queries.py
@@ -15,66 +15,83 @@ time_table_drop = f"DROP TABLE IF EXISTS {TIME_TABLE};"
 # CREATE TABLES
 
 songplay_table_create = """
-CREATE TABLE {} (
+CREATE TABLE IF NOT EXISTS {} (
     songplay_id int,
-    start_time time,
+    start_time bigint,
     user_id int,
     level text,
     song_id text,
     artist_id text,
     session_id int,
     location text,
-    user_agent text
+    user_agent text,
+    
+    PRIMARY KEY(songplay_id),
+    FOREIGN KEY(user_id) REFERENCES {}(user_id),
+    FOREIGN KEY(song_id) REFERENCES {}(song_id),
+    FOREIGN KEY(artist_id) REFERENCES {}(artist_id),
+    FOREIGN KEY(start_time) REFERENCES {}(start_time)
+
+    
 );
 """.format(
-    SONGS_PLAY_TABLE
+    SONGS_PLAY_TABLE, USERS_TABLE, SONGS_TABLE, ARTISTS_TABLE, TIME_TABLE
 )
 
+
 user_table_create = """
-CREATE TABLE {} (
+CREATE TABLE IF NOT EXISTS {} (
     user_id int,
     first_name text,
     last_name text,
-    gender boolean,
-    level text
+    gender text,
+    level text,
+
+    PRIMARY KEY(user_id)
 );
 """.format(
     USERS_TABLE
 )
 
 song_table_create = """
-CREATE TABLE {} (
+CREATE TABLE IF NOT EXISTS {} (
     song_id text,
     title text,
     artist_id text,
     year int,
-    duration real
+    duration real,
+
+    PRIMARY KEY(song_id)
 );
 """.format(
     SONGS_TABLE
 )
 
 artist_table_create = """
-CREATE TABLE {} (
+CREATE TABLE IF NOT EXISTS {} (
     artist_id text,
     name text,
     location text,
     latitude text,
-    longitude text
+    longitude text,
+
+    PRIMARY KEY(artist_id)
 );
 """.format(
     ARTISTS_TABLE
 )
 
 time_table_create = """
-CREATE TABLE {} (
-    start_time time,
+CREATE TABLE IF NOT EXISTS {} (
+    start_time bigint,
     hour int,
     day int,
     week int,
     month int,
     year int,
-    weekday boolean
+    weekday text,
+
+    PRIMARY KEY(start_time)
 )
 """.format(
     TIME_TABLE
@@ -84,7 +101,8 @@ CREATE TABLE {} (
 
 songplay_table_insert = """
 INSERT INTO {} (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) 
-VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)
+VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s) 
+ON CONFLICT(songplay_id) DO NOTHING;
 """.format(
     SONGS_PLAY_TABLE
 )
@@ -92,6 +110,7 @@ VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)
 user_table_insert = """
 INSERT INTO {} (user_id, first_name, last_name, gender, level) 
 VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT(user_id) DO NOTHING;
 """.format(
     USERS_TABLE
 )
@@ -99,13 +118,15 @@ VALUES(%s, %s, %s, %s, %s)
 song_table_insert = """
 INSERT INTO {} (song_id, title, artist_id, year, duration) 
 VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT(song_id) DO NOTHING;
 """.format(
     SONGS_TABLE
 )
 
 artist_table_insert = """
 INSERT INTO {} (artist_id, name, location, latitude, longitude) 
-VALUES(%s, %s, %s, %s, %s))
+VALUES(%s, %s, %s, %s, %s)
+ON CONFLICT(artist_id) DO NOTHING;
 """.format(
     ARTISTS_TABLE
 )
@@ -113,7 +134,8 @@ VALUES(%s, %s, %s, %s, %s))
 
 time_table_insert = """
 INSERT INTO {} (start_time, hour, day, week, month, year, weekday) 
-VALUES(%s, %s, %s, %s, %s), %s, %s)
+VALUES(%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT(start_time) DO NOTHING;
 """.format(
     TIME_TABLE
 )
@@ -121,21 +143,25 @@ VALUES(%s, %s, %s, %s, %s), %s, %s)
 # FIND SONGS
 
 song_select = """
+
+SELECT songs.song_id, artists.artist_id 
+FROM songs JOIN artists ON songs.artist_id = artists.artist_id
+WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
 """
 
 # QUERY LISTS
 
 create_table_queries = [
-    songplay_table_create,
     user_table_create,
     song_table_create,
     artist_table_create,
     time_table_create,
+    songplay_table_create,
 ]
 drop_table_queries = [
-    songplay_table_drop,
     user_table_drop,
     song_table_drop,
     artist_table_drop,
     time_table_drop,
+    songplay_table_drop,
 ]
